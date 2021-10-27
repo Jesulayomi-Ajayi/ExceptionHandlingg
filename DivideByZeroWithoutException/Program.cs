dfbe2a29b5dae2f4d277d45af5fdfa6e08dfb8ed
Jesulayomi Ajayi <jesulayoajayi@gmail.com>
2021-10-27 13:19:46 +0100

    Add project files.

---
+++ DivideByZeroWithoutException/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DivideByZeroWithoutException
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // get numerator
+            Console.Write("Please enter an integer numerator: ");
+            double numerator = Convert.ToDouble(Console.ReadLine());
+
+            // get denominator
+            Console.Write("Please enter an integer denominator: ");
+            double denominator = Convert.ToDouble(Console.ReadLine());
+
+            // divide the two integers, then display the result
+            double result = numerator / denominator;
+            Console.WriteLine("The result of " + numerator + " / " + denominator + " is: " + result);
+        }
+    }
+}
